0f3ef61b2f9d21cf974c6437b80d409248804f80
Megha <compchap.megha@gmail.com>
2018-02-26 19:22:06 -0800

    //TODO 2 : Create a method changeTextOnClick with no return value //TODO 3 : Change the text of textview_hello to "Learning Kotlin" inside the method

--- app/src/main/java/com/androidtutors/buttonclickandroidexample/MainActivity.java
+++ app/src/main/java/com/androidtutors/buttonclickandroidexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidtutors.buttonclickandroidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -33,5 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO 2 : Create a method changeTextOnClick with no return value
     //TODO 3 : Change the text of textview_hello to "Learning Kotlin" inside the method
+    public void changeTextOnClick(View v){
+        helloTextView.setText("Learning Kotlin");
+    }
 
 }
