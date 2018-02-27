95d502293d79e4c6e8ff29aceff662fdc7e438e9
Megha <compchap.megha@gmail.com>
2018-02-26 16:46:41 -0800

    Initial Code with One TextView and One Button

---
+++ app/src/main/java/com/androidtutors/buttonclickandroidexample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.androidtutors.buttonclickandroidexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView helloTextView;
+    private Button clickButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helloTextView = findViewById(R.id.textview_hello);
+        clickButton = findViewById(R.id.button_example);
+
+    }
+}
